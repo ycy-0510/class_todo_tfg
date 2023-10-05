aac848389ef4b1cdfe6157e48f9240d2dae73399
YCY <app97204@gmail.com>
2023-10-05 21:09:48 +0800

    add line notify

--- lib/logic/annouce_notifier.dart
+++ lib/logic/annouce_notifier.dart
@@ -3,6 +3,7 @@ import 'package:class_todo_list/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
 
 class AnnounceNotifier extends StateNotifier<AnnounceState> {
   late FirebaseFirestore db;
@@ -29,14 +30,21 @@ class AnnounceNotifier extends StateNotifier<AnnounceState> {
     );
   }
 
-  void sendData(String text) {
+  void sendData(String text) async {
     if (text.isNotEmpty) {
       try {
-        db.collection('announce').add({
+        DocumentReference dataRef = await db.collection('announce').add({
           'content': text,
           'userId': _ref.read(authProvider).user!.uid,
           'date': FieldValue.serverTimestamp()
         });
+        const String gasUrl =
+            'https://script.google.com/macros/s/AKfycbxVtO1mvVZMtu8QZBg2OjcZWfE4kNcAQlaC_UtY0QNwwwsMlJMcODrESWIeXkP1-QEuOQ/exec';
+        var res = await http
+            .get(Uri.parse('$gasUrl?announceId=${dataRef.id}&code=yccjyt'));
+        if (res.statusCode != 200 || res.body != '200') {
+          _showError('傳送Line錯誤');
+        }
       } catch (e) {
         _showError(e.toString());
       }
